93c5dd063e94700115fbc33e06a0c1da35796ff6
Eric Cronje <eric.cronje@gmail.com>
2024-07-30 20:01:55 +0800

    initial

---
+++ duckair/target/debug/deps/duckair.d
@@ -0,0 +1,5 @@
+C:\_FLAP03\GBZZBEBJ\Working\LearningRust\duckair\target\debug\deps\duckair.exe: src/main.rs
+
+C:\_FLAP03\GBZZBEBJ\Working\LearningRust\duckair\target\debug\deps\duckair.d: src/main.rs
+
+src/main.rs:
---
+++ duckair/target/debug/duckair.d
@@ -0,0 +1,1 @@
+C:\_FLAP03\GBZZBEBJ\Working\LearningRust\duckair\target\debug\duckair.exe: C:\_FLAP03\GBZZBEBJ\Working\LearningRust\duckair\src\main.rs
